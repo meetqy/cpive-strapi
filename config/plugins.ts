eac85b38ef34dbdb3f05df555bb8fe2d514a8907
meetqy <meetqy@qq.com>
2025-04-12 00:08:47 +0800

    chore: update dependencies in yarn.lock
    - Added new dependencies: available-typed-arrays@1.0.7, aws-sdk@2.1287.0, buffer@4.9.2, for-each@0.3.5, jmespath@0.16.0, possible-typed-array-names@1.1.0, punycode@1.3.2, querystring@0.2.0, sax@1.2.1, xml2js@0.4.19, xmlbuilder@9.0.7, uuid@8.0.0, and strapi-provider-cloudflare-r2@0.3.0.
    - Updated existing dependencies: base64-js, call-bind-apply-helpers, define-data-property, ieee754, is-arguments, is-callable, is-typed-array, isarray, has-property-descriptors, and util.
    - Removed deprecated or unused versions of some dependencies.

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,51 @@
-export default () => ({});
+const cloudflareR2 = (env) =>
+  process.env.NODE_ENV === "production"
+    ? {
+        provider: "strapi-provider-cloudflare-r2",
+        providerOptions: {
+          accessKeyId: env("CF_ACCESS_KEY_ID"),
+          secretAccessKey: env("CF_ACCESS_SECRET"),
+          /**
+           * `https://<ACCOUNT_ID>.r2.cloudflarestorage.com`
+           */
+          endpoint: env("CF_ENDPOINT"),
+          params: {
+            Bucket: env("CF_BUCKET"),
+          },
+          /**
+           * Set this Option to store the CDN URL of your files and not the R2 endpoint URL in your DB.
+           * Can be used in Cloudflare R2 with Domain-Access or Public URL: https://pub-<YOUR_PULIC_BUCKET_ID>.r2.dev
+           * This option is required to upload files larger than 5MB, and is highly recommended to be set.
+           * Check the cloudflare docs for the setup: https://developers.cloudflare.com/r2/data-access/public-buckets/#enable-public-access-for-your-bucket
+           */
+          cloudflarePublicAccessUrl: env("CF_PUBLIC_ACCESS_URL"),
+          /**
+           * Sets if all assets should be uploaded in the root dir regardless the strapi folder.
+           * It is useful because strapi sets folder names with numbers, not by user's input folder name
+           * By default it is false
+           */
+          pool: false,
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      }
+    : {};
+
+export default ({ env }) => ({
+  upload: {
+    config: {
+      ...cloudflareR2(env),
+      breakpoints: {
+        xxlarge: 1440, // 或者更大，适合超大显示器和电视
+        xlarge: 1280, // 大桌面显示器
+        large: 1024, // iPad Pro 横屏 或 笔记本电脑
+        medium: 768, // iPad 竖屏 或 大尺寸平板
+        small: 480, // 手机横屏 或 小尺寸平板
+        xsmall: 320, // 经典手机竖屏 （iPhone 5s 等）
+      },
+    },
+  },
+});
